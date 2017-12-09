fb3131db048bbb33f2883fb7bac977b5e7df1c0e
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-12-09 02:28:45 +0100

    Add metadata class

---
+++ pycoinmon/pycoinmon.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class Metadata:
+    def __init__(self):
+        self.__version__ = '0.0.1'
+        self.__author__ = 'Rubén de Celis Hernández'
+
+    def get_version(self):
+        return self.__version__
+
+    def get_author(self):
+        return self.__author__
+
+
+meta = Metadata()
+
