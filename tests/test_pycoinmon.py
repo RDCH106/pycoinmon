df53863df118cd7529ba3e91bc412a5cf6d57da0
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-10 16:34:54 +0100

    Add tests main

---
+++ tests/test_pycoinmon.py
@@ -0,0 +1,9 @@
+import unittest
+import pycoinmon as pcm
+
+class TestPycoinmon(unittest.TestCase):
+    def test_create(self):
+        self.assertEqual("r","r")
+
+if __name__ == '__main__':
+    unittest.main()
