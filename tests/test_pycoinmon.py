c6bf503f0f3287a15aafb3721a0efd4d67106aa8
jbarbadillo <javier.barbadillo@gmail.com>
2017-12-10 16:39:54 +0100

    Add print to test

--- tests/test_pycoinmon.py
+++ tests/test_pycoinmon.py
@@ -3,6 +3,7 @@ import pycoinmon as pcm
 
 class TestPycoinmon(unittest.TestCase):
     def test_create(self):
+        print("--test create--")
         self.assertEqual("r","r")
 
 if __name__ == '__main__':
