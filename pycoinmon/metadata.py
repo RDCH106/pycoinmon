a3fe8f8b788311c1d4f5ee906cf2a4c70ffac8db
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-12-19 22:48:29 +0100

    Add MANIFEST.in

--- pycoinmon/metadata.py
+++ pycoinmon/metadata.py
@@ -3,7 +3,7 @@
 
 class Metadata:
     def __init__(self):
-        self.__version__ = '0.2.5'
+        self.__version__ = '0.2.6'
         self.__author__ = 'Rubén de Celis Hernández, Javier Barbadillo'
 
     def get_version(self):
