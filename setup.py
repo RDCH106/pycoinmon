fbe57cefdef9054caf4411e8144cb6d4af8e34cc
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-12-19 22:35:20 +0100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,74 @@ def requirements():
 
     return requirements_list
 
+long_description = """"
+🐍 Python Port 🐍 Based on `COINMON`_
+
+    💰 Cryptocurrency price ticker CLI.
+
+Check cryptocurrencies’ prices, changes on your console. Best CLI tool
+for those who are both **Crypto investors** and **Engineers**.
+
+All data comes from `coinmarketcap.com`_ APIs.
+
+Installation
+------------
+
+You can install or upgrade pycoinmon with:
+
+``$ pip install pycoinmon --upgrade``
+
+Or you can install from source with:
+
+::
+
+    $ git clone https://github.com/RDCH106/pycoinmon.git --recursive
+    $ cd pycoinmon
+    $ pip install .
+
+Usage
+-----
+
+To check the top 10 cryptocurrencies ranked by their market cap, simply
+execute
+
+::
+
+    $ pycoinmon
+
+Options
+~~~~~~~
+
+You can use the ``-c`` (or ``--convert``) with the fiat currency symbol
+to find in terms of another currency. The default currency is USD and it
+supports AUD, BRL, CAD, CHF, CLP, CNY, CZK, DKK, EUR, GBP, HKD, HUF,
+IDR, ILS, INR, JPY, KRW, MXN, MYR, NOK, NZD, PHP, PKR, PLN, RUB, SEK,
+SGD, THB, TRY, TWD, ZAR.
+
+::
+
+    $ python pycoinmon -c eur // convert prices to Euro
+    $ python pycoinmon -c jpy // convert prices to Yen
+
+You can use the ``-t`` (or ``--top``) with the index to find the top n
+cryptocurrencies ranked by their market cap.
+
+::
+
+    $ python pycoinmon -t 50 // find top 50
+    $ python pycoinmon -t 1000 // find top 1000
+
+You can use the ``-h`` (or ``--help``) to find all valid options of
+pycoinmon
+
+::
+
+    $ python pycoinmon -h
+
+.. _COINMON: https://github.com/bichenkk/coinmon
+.. _coinmarketcap.com: https://coinmarketcap.com/
+    """
+
 
 setup(
     name = 'pycoinmon',
@@ -22,6 +90,7 @@ setup(
     version = metadata.get_version(),
     license = 'MIT',
     description = 'Python Port Based on COINMON',
+    long_description= long_description,
     url = 'https://github.com/RDCH106/pycoinmon',
     entry_points={
         'console_scripts': ['pycoinmon=pycoinmon.main:main'],
