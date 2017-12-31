acb4ed541d8030d6ba7a5c613d9c8bb3beabfff6
vgonisanz <vgonisanz@gmail.com>
2017-12-31 10:39:54 +0100

    codacy: Try to fix security issue executing os functionality

--- pycoinmon/core.py
+++ pycoinmon/core.py
@@ -57,12 +57,16 @@ class PyCoinmon(object):
         payload = {'limit': self.args.index, 'convert': self.args.currency}
         return get(self.sourceURL, params=payload)
 
+    @staticmethod
+    def do_clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def print_values(self):
         # Update values
         response = self.request_values()
 
         # Clear screen
-        os.system('cls' if os.name == 'nt' else 'clear')
+        self.do_clear_screen()
 
         # Redraw data
         print(process_title(ascii_title))
